1ac6f377d83fed4c34600243f84a6da534dc1cac
HemanthKumarBooragam <bhemanthkumar19@gmail.com>
2025-03-22 11:32:10 +0530

    #added language route

--- Vidly/Controllers/MovieController.cs
+++ Vidly/Controllers/MovieController.cs
@@ -9,18 +9,20 @@ namespace Vidly.Controllers
 {
     public class MovieController : Controller
     {
+        Movie movie = new Movie() { Id = 1, Name = "OG", Hero = "PSPK", Language = "Telugu" };
         // GET: Movie
         public ActionResult Random()
         {
-            var movie = new Movie() { Id = 1, Name = "OG"};
-
             return View(movie);
         }
 
         public ActionResult Hero()
         {
-            var movie = new Movie() { Id = 1, Name = "OG", Hero = "PSPK" };
+            return View(movie);
+        }
 
+        public ActionResult Language()
+        {
             return View(movie);
         }
     }
